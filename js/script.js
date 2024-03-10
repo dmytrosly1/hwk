cfd371eb15f4720fb3ca73ef1b6478c054ca6ce6
Dmitro <sirigamer22@gmail.com>
2024-03-10 08:56:06 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
-let name = prompt("Напишіть своє ім’я: ").trim();
-let age = Number(prompt("Напишіть свій вік: "));
-const smoke = confirm("Чи курите ви?");
+let name = prompt("Напишіть своє ім’я: ");
+let age;
+let smoke;
 
 function getUserInfo() {
-  while (name === "") {
-    name = prompt("Помилка! Напишіть своє ім’я ще раз.").trim();
+  while (name === "" || name === null) {
+    name = prompt("Помилка! Напишіть своє ім’я ще раз.");
   }
+
+  age = Number(prompt("Напишіть свій вік: "));
 
   while (isNaN(age) || age === "" || age === null || age > 105 || age < 12) {
     age = prompt("Помилка, неможливе значення віку. Напишіть свій вік ще раз.");
     age = Number(age);
   }
 
+  smoke = confirm("Чи курите ви?");
+
   if (age < 18 && smoke === false) {
     alert(`Молодець ${name}! Гарний приклад для своїх однолітків`);
   } else if (age < 18 && smoke === true) {
     alert(`${name}, а твої батьки знають про це?`);
-  } else if (age > 18 && smoke === false) {
+  } else if (age >= 18 && smoke === false) {
     alert(`Супер ${name}! Мабуть ще й спортом займаєшся!`);
-  } else if (age > 18 && smoke === true) {
+  } else if (age >= 18 && smoke === true) {
     alert(`Що ж ${name}, це твій вибір. Але я не радив би курити`);
   }
 }
-console.log(name, age, smoke);
 
 getUserInfo();
+
+console.log(name, age, smoke);
